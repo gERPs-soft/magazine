c08736dfdc3dffbf33cfbf67a23bcfeb01da69f2
Grzesiek Józefowicz <gjozef84@gmail.com>
2018-11-17 12:58:03 +0100

    Added entity: AssortmentGroup PackegeUnit Supplier Vat

--- src/main/java/com/gerps/magazine/entity/Assortment.java
+++ src/main/java/com/gerps/magazine/entity/Assortment.java
@@ -1,8 +1,48 @@
 package com.gerps.magazine.entity;
 
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
 /**
  * Created by Grzesiek on 2018-11-17
  */
+
+@Entity
+@NoArgsConstructor
 public class Assortment {
-    
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String assort_index;
+    private String name;
+
+    @ManyToOne
+    private AssortmentGroup assortmentGroupId;
+
+    private String PKWiU;
+    private String unit;
+    private String barcode;
+    private Double weightUnit;
+
+    @ManyToOne
+    private PackageUnit packageUnitId;
+
+    private Integer howManyInPackage;
+    private Integer height;
+    private Integer weight;
+    private Integer lenght;
+    private Integer howManyInPallet;
+
+    @ManyToOne
+    private Supplier supplierId;
+
+    private Long quantityInStock;
+    private BigDecimal priceLastSupply;
+
+    @Enumerated
+    private Vat vat;
 }
---
+++ src/main/java/com/gerps/magazine/entity/Supplier.java
@@ -0,0 +1,11 @@
+package com.gerps.magazine.entity;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by Grzesiek on 2018-11-17
+ */
+
+@Entity
+public class Supplier {
+}
