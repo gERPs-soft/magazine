fcd7e48349326cdc3e0af3640f9fc1651cfa2b45
Grzesiek Józefowicz <gjozef84@gmail.com>
2018-11-17 10:37:10 +0100

    Commit empty project

---
+++ src/main/java/com/gerps/magazine/MagazineApplication.java
@@ -0,0 +1,12 @@
+package com.gerps.magazine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagazineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagazineApplication.class, args);
+    }
+}
